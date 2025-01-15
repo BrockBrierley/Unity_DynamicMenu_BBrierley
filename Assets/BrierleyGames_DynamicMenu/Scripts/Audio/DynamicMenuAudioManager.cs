9706b3ea56c4fe0aedfd1c5d3d37005fea08c712
Brock Brierley <brock_brierley@outlook.com>
2025-01-15 13:18:28 +1100

    Created dynamic menu audio manager

---
+++ Assets/BrierleyGames_DynamicMenu/Scripts/Audio/DynamicMenuAudioManager.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+public class DynamicMenuAudioManager : MonoBehaviour
+{
+    //this object accessible from anywhere
+    public static DynamicMenuAudioManager _instnace;
+
+    //audio source to play sound
+    [Header("References")]
+    [SerializeField] private AudioSource audioSource;
+
+    //Audio clip lists
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip[] moveToAudio;
+    [SerializeField] private AudioClip[] moveBackAudio;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //ensure there isnt any doubling up of this static object instance
+        if (DynamicMenuAudioManager._instnace != null && DynamicMenuAudioManager._instnace != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Check to make sure audio source is valid
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    /// <summary>
+    /// Plays a random audio clip from a set list on this object
+    /// </summary>
+    public void PlayMoveToAudio()
+    {
+        //Stop audio playing
+        if(audioSource.isPlaying) audioSource.Stop();
+
+        //Get Random Clip
+        AudioClip clipToPlay = GetRandomClipFromArray(moveToAudio);
+
+        //return if audio is null
+        if (clipToPlay == null) return;
+
+        //Set Audio
+        audioSource.clip = clipToPlay;
+
+        //Play Audio
+        audioSource.Play();
+    }
+
+    /// <summary>
+    /// PLays a random audio clip from a set list on this object
+    /// </summary>
+    public void PlayMoveBackAudio()
+    {
+        //Stop audio playing
+        if (audioSource.isPlaying) audioSource.Stop();
+
+        //Get Random Clip
+        AudioClip clipToPlay = GetRandomClipFromArray(moveBackAudio);
+
+        //return if audio is null
+        if (clipToPlay == null) return;
+
+        //Set Audio
+        audioSource.clip = clipToPlay;
+
+        //Play Audio
+        audioSource.Play();
+    }
+
+    /// <summary>
+    /// Pass in a list of clips and play and random clip from the list
+    /// </summary>
+    /// <param name="listOfClips">List of clips to choose random audio from</param>
+    public void PlayAudio(AudioClip[] listOfClips)
+    {
+        //Stop audio playing
+        if (audioSource.isPlaying) audioSource.Stop();
+
+        //Get Random Clip
+        AudioClip clipToPlay = GetRandomClipFromArray(listOfClips);
+
+        //return if audio is null
+        if (clipToPlay == null) return;
+
+        //Set Audio
+        audioSource.clip = clipToPlay;
+
+        //Play Audio
+        audioSource.Play();
+    }
+
+    /// <summary>
+    /// Gets a list of audioclips and returns a random clip from the list
+    /// </summary>
+    /// <param name="listOfClips"></param>
+    /// <returns></returns>
+    public AudioClip GetRandomClipFromArray(AudioClip[] listOfClips)
+    {
+        //return null if list is empty
+        if (listOfClips.Length < 1)
+        {
+            return null;
+        }
+
+        //return random audio clip from list
+        return listOfClips[Random.Range(0, listOfClips.Length)];
+    }
+}
